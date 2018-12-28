85bf41bbcaa522c5c1c161ca9e7cd09f8625f963
Dhruv <dhruvchitkara27@gmail.com>
2018-12-29 01:21:24 +0530

    templates if else rendering

--- my-first-app/routes/index.js
+++ my-first-app/routes/index.js
@@ -4,7 +4,13 @@ const router = express.Router()
 router.get('/',(req,res,next) => {
   const data = {
     name: 'Index',
-    date: 'Dec 28, 2018'
+    date: 'Dec 28, 2018',
+    profiles: [
+      {name: 'Dhruv', city: 'Chandigarh', profession: 'Engineer'},
+      {name: 'Bhamti', city: 'Chandigarh', profession: 'Engineer'},
+      {name: 'Charvi', city: 'Chandigarh', profession: 'Engineer'},
+      {name: 'Jatin', city: 'Chandigarh'}
+    ]
   }
   res.render('index',data)
 })
